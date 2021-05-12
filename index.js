832ddc06bdede37f37bfbf052b0b45d3c788f1ab
Yanzz231 <75265517+Yanzz231@users.noreply.github.com>
2021-05-12 16:23:36 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const axios = require("axios");
-const fs = require("fs");
+const axios = require('axios');
+const fs = require('fs')
+const os = require('os');
+const clc = require('chalk');
 let _tembak = JSON.parse(fs.readFileSync(`./tembak.json`))
 
 for(let i = 0; i < 2000; i++){
-    
     axios.get(`https://h4ck3rs404-api.herokuapp.com/api/kuis/dareen?apikey=404Api`)
         .then((res) => {
             for (let a of _tembak) {
-                if (a.dare === res.data.result) {
-                    return
+                if (a.data === res.data.result) {
+                    return console.log(clc.greenBright('Ketemu Yang Sama'))
                 }
             }
             _tembak.push({
-                dare: res.data.result
+                data: res.data.result,
             })
             fs.writeFileSync(`./tembak.json`, JSON.stringify(_tembak, null, 2))
-            console.log('sukses +1 ke databasenya')
+            console.log(clc.greenBright('['+os.uptime+'] Sukses +1 ke databasenya'))
     })
 }
